ff69ec98ecfe946f4b600fce0d893bd992209a8b
dmrangel <outubrodavi@gmail.com>
2025-08-29 11:44:00 -0300

    exerc 2 broken

--- lua/main.lua
+++ lua/main.lua
@@ -1,3 +1,4 @@
+-- Exerc 1
 tabela = {
   "P1",
   "P2",
@@ -23,10 +24,34 @@ for i = 1, #tabela do
   print(tabela[i])
 end
 
+-- Exerc 2
 print("\nExerc 2\n")
 
+file = io.open("TESTE_LUA.txt", "r") if not file then print("Erro ao abrir o arquivo") end
 
+table = {}
+cabecalho = file:read("*l")
+unidades = file:read("*l")
+column_list = {}
+column = string.match(cabecalho, "%S+")
 
+for column in string.gmatch(cabecalho, "%S+") do
+  table[column] = {}
+  column_list[#column_list+1] = column
+  if column ~= "DATA" then
+    unidade = string.match(unidades, "%S+")
+    table[column] = unidade
+  end
+end
+for linha in file:lines() do
+  data = string.match(linha, "(%d%d%d%d/%d%d/%d%d)")
+  print(data)
+  table["DATA"][#table["DATA"]+1] = data
+  for i = 2, #column_list do
+    value = string.match(linha, "%d+%.%d")
+    print(value)
+    table[column_list[i]][#table[column_list[i]]+1] = value
+  end
+end
 
-
-
+file:close()
