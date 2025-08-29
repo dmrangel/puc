76f0ef2507c6a054a591cbb4914b3b25416a3ee2
dmrangel <outubrodavi@gmail.com>
2025-08-29 13:04:10 -0300

    exerc 2 working

--- lua/main.lua
+++ lua/main.lua
@@ -33,24 +33,32 @@ table = {}
 cabecalho = file:read("*l")
 unidades = file:read("*l")
 column_list = {}
-column = string.match(cabecalho, "%S+")
 
 for column in string.gmatch(cabecalho, "%S+") do
   table[column] = {}
   column_list[#column_list+1] = column
   if column ~= "DATA" then
     unidade = string.match(unidades, "%S+")
-    table[column] = unidade
+    table[column].unidade = unidade
   end
 end
 for linha in file:lines() do
   data = string.match(linha, "(%d%d%d%d/%d%d/%d%d)")
-  print(data)
   table["DATA"][#table["DATA"]+1] = data
   for i = 2, #column_list do
     value = string.match(linha, "%d+%.%d")
-    print(value)
     table[column_list[i]][#table[column_list[i]]+1] = value
+  end
+end
+
+for n = 1, #column_list do
+  column = column_list[n]
+  print("\nColuna: "..column)
+  if column ~= "DATA" then
+     print("Unidade: "..table[column_list[n]].unidade)
+  end
+  for i = 1, #table[column] do
+    print(table[column][i])
   end
 end
 
