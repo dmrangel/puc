1fd00d571ed1ec95f94f2b0e14079328b65324a0
dmrangel <outubrodavi@gmail.com>
2025-09-02 11:06:53 -0300

    exerc 1.2 done

--- inf1026/dict/dict.py
+++ inf1026/dict/dict.py
@@ -1,17 +1,35 @@
 def cria_dict(arq):
     dic = dict()
-    lmatriculas = []
     for line in arq:
         materias = []
         dados = line.strip().split(";")
         matricula = dados[0]
         materias = dados[1:]
-        lmatriculas.append(matricula)
         dic[matricula] = materias
-    return dic, lmatriculas
+    return dic
+
+def cria_dict_inverso(dic):
+    inv_dic = dict()
+    ldisciplinas = []
+    for matricula in dic:
+        for d in dic[matricula]:
+            if d not in ldisciplinas:
+                inv_dic[d] = [matricula]
+                ldisciplinas.append(d)
+            else:
+                inv_dic[d].append(matricula)
+    return inv_dic
+
 
 arq = open("inscritos.csv", "r")
-dic, lmatriculas = cria_dict(arq)
+dic = cria_dict(arq)
+arq.close()
 
-for matricula in lmatriculas:
+for matricula in dic:
     print(f"Matrícula: {matricula} | Disciplinas: {dic[matricula]}\n")
+
+print("==========================================================\n")
+
+inv_dic = cria_dict_inverso(dic)
+for d in inv_dic:
+    print(f"Disciplina: {d} | Matrículas: {inv_dic[d]}\n")
